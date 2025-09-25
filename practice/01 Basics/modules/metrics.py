915e47019d0fa57c7a2eb76a9eea73510b8f710f
FurnoLiberto <144216567+FurnoLiberto@users.noreply.github.com>
2025-09-25 15:55:19 +0500

    Update metrics.py

--- practice/01 Basics/modules/metrics.py
+++ practice/01 Basics/modules/metrics.py
@@ -48,45 +48,44 @@ def norm_ED_distance(ts1: np.ndarray, ts2: np.ndarray) -> float:
     return norm_ed_dist
 
 
-def DTW_distance(ts1: np.ndarray, ts2: np.ndarray, r: float = 1) -> float:
-    """
+def DTW_distance(ts1: np.ndarray, ts2: np.ndarray, r: float = 1) -> float:   
+    """    
     Calculate DTW distance
 
     Parameters
-    ----------
     ts1: first time series
     ts2: second time series
-    r: warping window size
-    
+    r: warping window size (as a percentage of the time series length)
+
     Returns
-    -------
     dtw_dist: DTW distance between ts1 and ts2
     """
 
-    dtw_dist = 0
+    n = len(ts1)
+    m = len(ts2)
+    
+    # Вычисляем размер окна поиска в абсолютных значениях
+    r = int(np.ceil(n * r))
 
-    # INSERT YOUR CODE
-    m, n = len(ts1), len(ts2)
-    
-    # Матрица накопления расстояний
-    D = np.zeros((m + 1, n + 1)) + np.inf
-    D[0][0] = 0
-    
-    # Если окно не задано, применяем полное заполнение матрицы
-    if r is None or r >= max(m, n):
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                cost = (ts1[i - 1] - ts2[j - 1]) ** 2
-                D[i][j] = cost + min(D[i - 1][j], D[i][j - 1], D[i - 1][j - 1])
-    else:
-        # Ограничиваем окно размерами (+/- r вокруг диагонали)
-        for i in range(1, m + 1):
-            lower_bound = max(1, i - r)
-            upper_bound = min(n, i + r)
-            
-            for j in range(lower_bound, upper_bound + 1):
-                cost = (ts1[i - 1] - ts2[j - 1]) ** 2
-                D[i][j] = cost + min(D[i - 1][j], D[i][j - 1], D[i - 1][j - 1])
-                
-    return D[m][n]
-    #return dtw_dist
+    # Создаем матрицу dtw размером (n+1) x (m+1) и инициализируем ее значениями бесконечности
+    dtw_matrix = np.full((n + 1, m + 1), np.inf)
+    # Инициализируем начальный элемент нулевым значением
+    dtw_matrix[0, 0] = 0
+
+    # Заполняем матрицу dtw, используя динамическое программирование
+    for i in range(1, n + 1):
+        # Определяем границы окна поиска
+        start = max(1, i - r)
+        end = min(m + 1, i + r + 1)
+        for j in range(start, end):
+            # Вычисляем евклидово расстояние между текущими элементами временных рядов
+            cost = (ts1[i-1] - ts2[j-1])**2 
+            # Вычисляем минимальную стоимость из соседних ячеек
+            last_min = min(dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1])
+            # Записываем общую стоимость в текущую ячейку
+            dtw_matrix[i, j] = cost + last_min
+
+    # DTW расстояние - это значение в правом нижнем углу матрицы
+    dtw_dist = dtw_matrix[n, m]
+
+    return dtw_dist
