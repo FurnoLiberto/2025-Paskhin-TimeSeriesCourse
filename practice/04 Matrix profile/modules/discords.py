2864b914c3576cad14316510073ec0de8aa47214
FurnoLiberto <144216567+FurnoLiberto@users.noreply.github.com>
2025-11-04 23:24:42 +0500

    Update discords.py

--- practice/04 Matrix profile/modules/discords.py
+++ practice/04 Matrix profile/modules/discords.py
@@ -23,6 +23,26 @@ def top_k_discords(matrix_profile: dict, top_k: int = 3) -> dict:
 
     # INSERT YOUR CODE
 
+    mp_copy = matrix_profile['mp'].copy()
+    profile_indices = matrix_profile['mpi']
+    excl_zone = matrix_profile['excl_zone']
+
+    for _ in range(top_k):
+        # Находим индекс максимального значения
+        max_idx = np.argmax(mp_copy)
+
+        if np.isinf(mp_copy[max_idx]):
+            break
+
+        # Сохраняем индекс и расстояние
+        discords_idx.append(max_idx)
+        discords_dist.append(mp_copy[max_idx])
+
+        # Сохраняем индекс ближайшего соседа
+        discords_nn_idx.append(profile_indices[max_idx])
+
+        # Применяем зону исключения, чтобы не найти соседей
+        apply_exclusion_zone(mp_copy, max_idx, excl_zone, -np.inf) # Заменяем на -inf
     return {
         'indices' : discords_idx,
         'distances' : discords_dist,
